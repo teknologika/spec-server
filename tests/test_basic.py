af6a557a869f336f30572116a90b9b2b9e5be048
Bruce McLeod <bruce@teknologika.com>
2025-07-15 23:28:22 +1000

    updated test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,7 +11,7 @@ from spec_server.server import create_server
 
 def test_version():
     """Test that version is defined."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "v0.1.0"
 
 
 def test_author():
